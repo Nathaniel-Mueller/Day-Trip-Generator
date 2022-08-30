f84b8a814cc0470af88c9e1d19798d1e1051f4c6
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-08-30 00:28:52 -0700

    Added all lists and assigned variables to a random selection from each list

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+import random
+
+destinationList = ["Greece", "Sweden", "Germany", "Australia", "Japan", "Italy"]
+restaurantList = ["Applebees", "Olive Garden", "Red Lobster",  "LG's Prime Steakhouse", ]
+transportationList = ["Bus", "Car", "Train", "Monorail"]
+entertainmentList = ["Movie", "Board Games", "Bowling", "Hiking", "Concert", "Theater"]
+randomDestination = random.choice(destinationList)
+randomRestaurant = random.choice(restaurantList)
+randomTransport = random.choice(transportationList)
+randomentertainment = random.choice(entertainmentList)
