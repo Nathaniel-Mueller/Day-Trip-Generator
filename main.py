b6d40bf770f9f386cb1269c1c6f158c7281904fd
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-08-30 02:55:26 -0700

    Added new function to get incorrect inputs and ask for a new input. Added option to reroll again until negative input is given.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 destinationList = ["Greece", "Sweden", "Germany", "Australia", "Japan", "Italy"]
 restaurantList = ["Applebees", "Olive Garden", "Red Lobster",  "LG's Prime Steakhouse", ]
 transportationList = ["Bus", "Car", "Train", "Monorail"]
@@ -8,28 +9,68 @@ randomDestination = random.choice(destinationList)
 randomRestaurant = random.choice(restaurantList)
 randomTransport = random.choice(transportationList)
 randomEntertainment = random.choice(entertainmentList)
+def incorrectInputReturn (inputStr):
+    if inputStr == "destination" or inputStr == "restaurant" or inputStr == "transportation" or inputStr == "entertainment":
+        pass
+    else:
+        while inputStr != "destination" and inputStr != "restaurant" and inputStr != "transportation" and inputStr != "entertainment":
+            inputStr = input("I'm sorry that's an incorrect input, please try again! ")
+    output = inputStr
+    return output
+
 def reselect (reroll):
     while reroll != "destination" or reroll != "restaurant" or reroll != "transportation" or reroll != "entertainment":
         if reroll == "destination":
             newDestination = random.choice(destinationList)
-            print (f"Your new destination is {newDestination}!")
             return newDestination
         elif reroll == "restaurant":
             newRestaurant = random.choice(restaurantList)
-            print(f"Your new restaurant choice is {newRestaurant}!")
             return newRestaurant
         elif reroll == "transportation":
             newTransport = random.choice(transportationList)
-            print(f"Your new form of transportation is {newTransport}!")
             return newTransport
         elif reroll == "entertainment":
             newEntertainment = random.choice(entertainmentList)
-            print(f"Your new entertainment choice is {newEntertainment}!")
             return newEntertainment
         else:
             reroll = input("Incorrect input, please try again! ")
-            reroll.casefold()
+            reroll = reroll.casefold()
 
+print(f"""Hello user! Your randomly selected day trip is:
+Destination: {randomDestination}
+Restaurant: {randomRestaurant}
+Transportation: {randomTransport}
+Entertainment: {randomEntertainment}""")
 
+print("")
 
-
+willReroll = input("Would you like to reroll any of these options? Please enter yes or no. ")
+willReroll = willReroll.casefold()
+print ("")
+while willReroll != "yes" and willReroll != "no":
+    willReroll = input("I'm sorry, that's an incorrect input, please try again! ")
+    willReroll = willReroll.casefold()
+while willReroll == "yes":
+    reselectChoice = input("Which option would you like to reroll? ")
+    reselectChoice = reselectChoice.casefold()
+    reselectChoice = incorrectInputReturn (reselectChoice)
+    reselectChoice = reselectChoice.casefold()
+    newChoice = reselect(reselectChoice)
+    if reselectChoice == "restaurant":
+        randomRestaurant = newChoice
+    elif reselectChoice == "destination":
+        randomDestination = newChoice
+    elif reselectChoice == "transportation":
+        randomTransport == newChoice
+    elif reselectChoice == "entertainment":
+        randomEntertainment = newChoice
+    print(f"Your new {reselectChoice} selection is {newChoice}!")
+    willReroll = input("Would you like to reroll another option? ")
+    willReroll = willReroll.casefold()
+if willReroll == "no":
+    print(f"""Your completed randomly selected day trip is:
+Destination: {randomDestination}
+Restaurant: {randomRestaurant}
+Transportation: {randomTransport}
+Entertainment: {randomEntertainment}""")
+dayComplete = print("Type 'yes' to confirm or 'no' to go back and change something. ")
